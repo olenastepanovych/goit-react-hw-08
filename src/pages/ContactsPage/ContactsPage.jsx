9fbf13a0492b6d0e295cd2c53c8343362a6cd530
Lena <stlena113@gmail.com>
2025-05-13 18:46:47 +0300

    third  commit

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import ContactForm from '../../components/ContactForm/ContactForm';
 import ContactList from '../../components/ContactList/ContactList';
 import SearchBox from '../../components/SearchBox/SearchBox';
+import { fetchContacts } from '../../redux/contacts/operations';
 import styles from './ContactsPage.module.css';
 
 const ContactsPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Phonebook</h1>
@@ -15,3 +24,4 @@ const ContactsPage = () => {
 };
 
 export default ContactsPage;
+
